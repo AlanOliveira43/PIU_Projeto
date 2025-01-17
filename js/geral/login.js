1ca49660017aff21f88258984d97cd3d670623cc
Alan Oliveira <castro_alan11@hotmail.com>
2025-01-17 14:04:18 -0300

    Feature landing page

---
+++ js/geral/login.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    // Redireciona o usuário para a página index.html
+window.location.href = '../../index.html';
+});
--- js/geral/logout.js
+++ js/geral/logout.js
@@ -1,4 +1,4 @@
 document.getElementById('logout-btn').addEventListener('click', function () {
     // Redireciona o usuário para a página index.html
-window.location.href = '../../index.html';
+window.location.href = '../../index-landing.html';
 });
